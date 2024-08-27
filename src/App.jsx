7c77606dfa3e624c373ea06e127d442a4110aeaa
AdsonTanajura <adson.tanajura@gmail.com>
2024-08-27 11:25:33 -0300

    A geracao dos produto de forma dinamica

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
             price: 119.99,
             active: false,
             quantity: 1, 
-            isInBag: false
+            isInBag: true,
         },
         {
             id: 2, 
@@ -86,12 +86,37 @@ function App() {
         }
     ];
 
+
+
     return ( 
         <>
             <section className="items">
                 <h4>Jersey Shop Made with React JS</h4>
+                {
+                    items.map(item => 
+                        <div key={item.id} className={`product ${item.isInBag ? 'selected': '' }`}>
+                            <div className="photo">
+                                <img src={"././img/" + item.photo} />
+                            </div>
+                            <div className="description">
+                                <span className="name">{item.name}</span>
+                                <span className="price">{item.price}</span>
+                                {
+                                    item.isInBag &&
+                                    <div className="quantity-area">
+                                        <button>-</button>
+                                        <span className="quantity">{item.quantity}</span>
+                                        <button>+</button>
+                                    </div>
+                                    
+                                }
+                            </div>
+                        </div>
+                    )
+                }
                 
-                <div className="product selected">
+
+                {/* <div className="product selected">
                     <div className="photo">
                         <img src="././img/real_madrid.webp" />
                     </div>
@@ -217,7 +242,7 @@ function App() {
                             <button>+</button>
                         </div>
                     </div>
-                </div>
+                </div> */}
             </section>
             
 
